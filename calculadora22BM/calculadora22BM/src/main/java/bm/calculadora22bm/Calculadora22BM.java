1bb6b1c363907623cf919268a6515c281e068b3a
Crissper1 <165078997+Crissper1@users.noreply.github.com>
2024-06-22 13:01:38 -0300

    calculadorJavaSwing
    Trabajo en clase 2BM 2024

---
+++ calculadora22BM/calculadora22BM/src/main/java/bm/calculadora22bm/Calculadora22BM.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package bm.calculadora22bm;
+
+/**
+ *
+ * @author leo
+ */
+public class Calculadora22BM {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
